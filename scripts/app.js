7210d37aa703b35152a32c2b072f0b6cf90e3ec9
Dmytro Doroshko <dmitry.doroshko.99@gmail.com>
2025-03-15 16:40:23 +0200

    Create a mobile nav menu on mobile screens and lower.

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,12 @@
 const hamburgerMenu = document.querySelector("#hamburger-menu");
+const mobileNav = document.querySelector("#mobile-nav");
 
 hamburgerMenu.addEventListener("click", () => {
-  if (hamburgerMenu.classList.contains("active")) {
+  if (hamburgerMenu.classList.contains("active") || mobileNav.classList.contains("active")) {
     hamburgerMenu.classList.remove("active");
+    mobileNav.classList.remove("active");
   } else {
     hamburgerMenu.classList.add("active");
+    mobileNav.classList.add("active");
   }
 });
